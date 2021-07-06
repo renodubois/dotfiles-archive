73a0dbf341fb9dc14383fc4ccc56a0435ed89a4a
Reno DuBois <me@renodubois.com>
2021-07-06 13:09:24 -0500

    Base fish config

---
+++ .config/fish/config.fish
@@ -0,0 +1,18 @@
+# Remove default fish greeting
+set fish_greeting ""
+
+# NOTE: this might be bad
+set -gx COLORTERM truecolor
+set -gx EDITOR nvim
+
+# PATH Modifications
+set -x PATH $PATH /home/reno/.bin/
+set -x PATH $PATH /home/reno/.cargo/bin
+
+# Aliases
+function ls
+	command ls -la --color=auto $argv
+end
+function evim
+	command nvim /home/reno/.config/nvim/init.vim
+end
